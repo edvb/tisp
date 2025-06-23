a6b6a07cf613f2ad34325d5f05dc853d92734531
Ed van Bruggen <ed@edryd.org>
2025-06-23 13:04:41 -0400

    Change CC defines to set C99 standard

--- config.mk
+++ config.mk
@@ -4,14 +4,15 @@ VERSION = 0.1
 # paths
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
+LIBPREFIX = $(PREFIX)/lib/tisp/pkgs/std
 
 # includes and libraries
 INCS = -Iinclude
 LIBS = -lm -ldl
 
 # flags
-CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=600
-CFLAGS = -std=c99 -O3 -pedantic -Wall -fPIC $(INCS) $(CPPFLAGS)
+DEFINES = -DVERSION=\"$(VERSION)\" -D_POSIX_C_SOURCE=200809L
+CFLAGS  = -std=c99 -O3 -pedantic -Wall -fPIC $(INCS) $(DEFINES)
 LDFLAGS = -O3 -Wl,-rpath=$(DESTDIR)$(PREFIX)/lib/tisp $(LIBS)
 
 # turn off debug mode by default
